c6196e0391bc2458372f0dcdc9659e6c7bc4559b
paultmeuser <paul.tmeuser@gmail.com>
2025-09-22 20:38:16 -0400

    Patch stderr in cli tests to reduce noise in test output

--- cli/test_ledger_shell.py
+++ cli/test_ledger_shell.py
@@ -1,14 +1,14 @@
-import unittest
+from unittest import TestCase, mock
 from io import StringIO
 
 from parameterized import parameterized
 from cli.ledger_shell import LedgerShell
 from models.account import Account, AccountType
 
-class TestLedgerShell(unittest.TestCase):
+class TestLedgerShell(TestCase):
 
     def setUp(self):
-        self.mock_ledger = unittest.mock.MagicMock()
+        self.mock_ledger = mock.MagicMock()
         self.shell = LedgerShell(self.mock_ledger)
 
     
@@ -27,14 +27,15 @@ class TestLedgerShell(unittest.TestCase):
         ("",),               # Empty input
     ])
     def test_add_account_invalid_format(self, command_arg_list):
-        self.shell.do_add_account(command_arg_list)
-        self.mock_ledger.add_account.assert_not_called()
+        with mock.patch('sys.stderr', new_callable=StringIO) as mock_stderr:
+            self.shell.do_add_account(command_arg_list)
+            self.mock_ledger.add_account.assert_not_called()
 
     def test_get_account(self):
         account = Account(id=1, name="Cash", type=AccountType.DEBIT, description="")
         self.mock_ledger.get_account.return_value = account
 
-        with unittest.mock.patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+        with mock.patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             self.shell.do_get_account("1")
             self.mock_ledger.get_account.assert_called_once_with(1)
             self.assertIn(str(account), mock_stdout.getvalue())
